04efaf11898a78afdd4fa66eef47e1a2da5b3e54
Christian <explosivfibres@hotmail.com>
2024-12-09 10:00:10 +0100

    setup

--- strajk-bowling/vite.config.js
+++ strajk-bowling/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import { configDefaults } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './setupTests.js',
+    alias: {
+      '\\.(css|less|scss|sass)$': './__mocks__/styleMock.js',
+    },
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'lcov'],
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+});
