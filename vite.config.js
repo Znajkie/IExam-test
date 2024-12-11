c6e51dd077cafb7ad723303c18b15a81e4ba7f71
Christian <explosivfibres@hotmail.com>
2024-12-11 13:56:29 +0100

    e

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({jsxRuntime: 'automatic'})],
+  plugins: [react({ jsxRuntime: 'automatic' })],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './setupTests.js',
-    alias: {
-      '\\.(css|less|scss|sass)$': './__mocks__/styleMock.js',
-    },
+    css: false,
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'lcov'],
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80,
+      reportsDirectory: './coverage',
+      reporter: ['text', 'json', 'json-summary'],
+      all: true,
+      exclude: ['./src/main.jsx', 'vite.config.js'],
     },
   },
 });
